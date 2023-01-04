92576d42fb3b49c270cd581020bdf04c77583048
murat.dogan2 <murat.dogan2@ERKHBN0096>
2023-01-04 16:19:09 +0300

    Add project files.

---
+++ 180822_Kronometre/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace _180822_Kronometre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(124, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BAŞLAT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaat.Location = new System.Drawing.Point(99, 30);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(75, 54);
+            this.lblSaat.TabIndex = 1;
+            this.lblSaat.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(180, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 54);
+            this.label2.TabIndex = 2;
+            this.label2.Text = ":";
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDakika.Location = new System.Drawing.Point(225, 30);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(75, 54);
+            this.lblDakika.TabIndex = 4;
+            this.lblDakika.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(306, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 54);
+            this.label4.TabIndex = 3;
+            this.label4.Text = ":";
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaniye.Location = new System.Drawing.Point(348, 30);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(75, 54);
+            this.lblSaniye.TabIndex = 6;
+            this.lblSaniye.Text = "00";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 226);
+            this.Controls.Add(this.lblSaniye);
+            this.Controls.Add(this.lblDakika);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSaat);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "KRONOMETRE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSaniye;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 180822_Kronometre/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _180822_Kronometre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int saat = 0, dakika = 0, saniye = 0;
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            saniye++;
+            if (saniye > 59)
+            {
+                dakika++;
+                if (dakika > 59)
+                {
+                    saat++;
+                    lblSaat.Text =saat.ToString();
+                    dakika = 0;
+                    
+                }
+                lblDakika.Text = dakika < 10 ? string.Format("0{0}",dakika): dakika.ToString();
+                saniye = 0;
+            }
+           //tek satırda if else yazma işlemeine "Turnery If" denir.?=if tir, :ise else yerine kullanılır
+            lblSaniye.Text = saniye < 10 ? string.Format("0{0}", saniye):saniye.ToString();
+         /*   if (saat == dakika)
+            {
+                MessageBox.Show("Biri Seni Düşünüyor.");
+            }
+         */
+            /*   if (saniye<10)
+            {
+                lblSaniye.Text = string.Format("0{0}", saniye);
+            }
+            else
+            {
+                lblSaniye.Text=saniye.ToString();
+            }
+            */
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+       
+    }
+}
